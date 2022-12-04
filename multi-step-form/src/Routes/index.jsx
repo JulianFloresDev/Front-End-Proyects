ae49f634bd3e5a6a810b7c43228cec5d0869e141
Julian Flores <julianfloresdev@gmail.com>
2022-12-03 22:46:44 -0300

    03: add general styles and some format tools

--- multi-step-form/src/Routes/index.jsx
+++ multi-step-form/src/Routes/index.jsx
@@ -1,7 +1,9 @@
+import styles from './layout.module.css';
+
 const Layout = () => {
   return (
     <main>
-      <h1>Multi Steps Form App</h1>
+      <h1 className={styles.mainTitle}>Multi Steps Form App</h1>
     </main>
   );
 };
